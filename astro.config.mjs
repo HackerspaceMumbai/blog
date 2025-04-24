d3c8044d3cbfee4ca8aece95e44f3ccd951960c5
Augustine Correa <augcor@gmail.com>
2025-04-24 14:35:55 +0530

    feat: :sparkles: Ensure look and feel is closest to old site
    Add layout components and integrate Tailwind CSS for styling
    
    Fixes #5

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
